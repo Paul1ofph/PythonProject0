610babc59ec4147d1f5775218fba65f089204ffa
Paul1ofph <127723523+Paul1ofph@users.noreply.github.com>
2023-03-24 13:01:35 +0100

    Update Checker.py

--- Checker.py
+++ Checker.py
@@ -1,15 +1,12 @@
 password = input("Enter password: ")
-
 has_uppercase = False
 has_number = False
-
-if len(password) >= 8:
+While len(password) >= 8:
     for c in password:
         if c.isupper():
             has_uppercase = True
         elif c.isdigit():
             has_number = True
-
     if has_uppercase and has_number:
         print("Strong password!")
     elif has_uppercase or has_number:
